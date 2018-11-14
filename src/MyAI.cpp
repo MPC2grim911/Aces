bed8adbd5d9e32aa815b0262450b28e7cb81f07d
MPC2grim911 <murphc1@uci.edu>
2018-11-13 19:06:32 -0800

    updated vector version
    note this makes an executable but creates an error while executing

--- src/MyAI.cpp
+++ src/MyAI.cpp
@@ -32,14 +32,18 @@ MyAI::MyAI() : Agent()
 	goBack = false;
 	moves = 0;
 	
-	xPos = 0; //player initial position, change if needed
-	yPos = 0;
+	Pos.x = 0; //player initial position, change if needed
+	Pos.y = 0;
 	dir = 0; //0 is E, 1 is N, 2 is W, 3 is S at the moment
 	
 	xWall = false; // true when bump into wall and establishes limits
 	yWall = false;
 	xLim = -1; //changed when bump
 	yLim = -1;
+	
+	vector<Point> safe;
+	vector<Point> testPos;
+	vector<Point> explore;
 	
 	// ======================================================================
 	// YOUR CODE ENDS
@@ -137,6 +141,51 @@ Agent::Action MyAI::getAction
 
 		if (!stench && !breeze)
 		{
+			safe.push_back(Pos);
+			
+			if(Pos.y != yLim){
+				Point n = Pos;
+				n.y += 1;
+				testPos.push_back(n);
+			}
+			if(Pos.y != 0){
+				Point s = Pos;
+				s.y -= 1;
+				testPos.push_back(s);
+			}
+			if(Pos.x != xLim){
+				Point e = Pos;
+				e.x += 1;
+				testPos.push_back(e);
+			}
+			if(Pos.x != 0){
+				Point w = Pos;
+				w.x -= 1;
+				testPos.push_back(w);
+			}
+			
+			for(int i = 0; i < testPos.size(); i++){
+				for(int j = 0; j < safe.size(); j++){
+					if(testPos.at(i) == safe.at(j)){
+						testPos.erase(testPos.begin() + i);;
+					}
+				}
+			}
+			
+			for(int i = 0; i < testPos.size(); i++){
+				bool same = false;
+				for(int j = -; j < explore.size(); j++){
+					if(testPos.at(i) == explore.at(j)){
+						same = true;
+						break;
+					}
+				}
+				if(!same)
+					explore.push_back(testPos.at(i));
+			}
+			
+			testPos.clear();
+			
 			retrace.push(FORWARD);
 			moves++;
 			if(dir == 0)//player position changes
@@ -147,6 +196,18 @@ Agent::Action MyAI::getAction
 				xPos -= 1;
 			if(dir == 3)
 				yPos -= 1;
+				
+			cout << "Safe points: " << endl;
+			for(int i = 0; i < safe.size(); i++){
+				Point sf = safe.at(i);
+				cout << "\t" << sf.x << ", " << sf.y << endl;
+			}
+			cout << "Explore points: " << endl;
+			for(int j = 0; j < explore.size(); j++){
+				Point ex = explore.at(j);
+				cout << "\t" << ex.x << ", " << ex.y << endl;
+			}
+			
 			return FORWARD;
 		}
 	}
@@ -161,7 +222,9 @@ Agent::Action MyAI::getAction
 // ======================================================================
 // YOUR CODE BEGINS
 // ======================================================================
-
+bool MyAI::Point::&operator==(const Point &p) const{
+	return(this->x == p.x) && (this->y == p.y);
+}
 
 // ======================================================================
 // YOUR CODE ENDS
